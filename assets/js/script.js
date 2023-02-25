54e845c550c585b21d8b455b043023b33d5a08fc
tlaksono7 <tito.laksono@gmail.com>
2023-02-25 12:51:32 +0700

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+const adviceId = document.querySelector('#advice-id')
+const adviceText = document.querySelector('.advice-body__text')
+const rerollButton = document.querySelector('.advice-reroll__button')
+const rerollIcon = document.querySelector('.advice-roll_button--image')
+
+let url = 'https://api.adviceslip.com/advice'
+
+const callAPI = function() {
+    fetch(url, { cache: "no-cache" })
+    .then(response => response.json())
+    .then(data => 
+        processAdvice(data)
+    );
+}
+
+const processAdvice = function(data) {
+    adviceId.textContent = data.slip.id
+    adviceText.textContent = `“${data.slip.advice}”`
+}
+
+rerollButton.addEventListener('click', function() {
+    adviceId.textContent = ""
+    adviceText.innerHTML = `<div class="lds-ring"><div></div><div></div><div></div>`
+    setTimeout(callAPI, 1000)
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    callAPI()
+});
